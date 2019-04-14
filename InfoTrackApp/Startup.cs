d0b7a091bdf12c332e799d912e10122414e4f405
Flávio Cadete <flavio_cadete@hotmail.com>
2019-04-14 10:21:28 +0100

    Add Swagger UI Generator

--- InfoTrackApp/Startup.cs
+++ InfoTrackApp/Startup.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace InfoTrackApp
 {
@@ -27,6 +27,11 @@ namespace InfoTrackApp
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "InfoTrack.API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +47,13 @@ namespace InfoTrackApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "InfoTrack.API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
